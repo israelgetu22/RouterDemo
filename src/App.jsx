a9ed9754fcaad4ba4c08623f5849a708d66b6cd1
Isarel Getu <israelgetu101@gmail.com>
2022-05-11 23:58:40 -0500

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Team from "./components/Team";
+import Details from "./components/Detail";
 
 function App() {
-  return <p></p>;
+  return (
+    <div>
+      <header>
+        <Link to="/"> Home</Link>
+        <Link to="/team"> Team</Link>
+        <Link to="/details">Details</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
